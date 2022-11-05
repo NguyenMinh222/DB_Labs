62812122a4f6b0bac23a1570301c290db97e7efc
NguyenMinh222 <79207246+NguyenMinh222@users.noreply.github.com>
2022-11-05 20:58:01 +0300

    Update ThirdLab.sql

--- ThirdLab.sql
+++ ThirdLab.sql
@@ -37,3 +37,31 @@ dateRelease varchar(16) not null,
 validUntil varchar(16) not null,
   constraint  check_validUntil check (validUntil in ('.','1','2','3','4','5','6','7','8','9','0'))
  );
+ 
+create table contact
+(
+id_contact int(4) auto_increment primary key,
+email varchar(32) not null unique,
+urAdress varchar(32) not null,
+phoneNumber varchar(16) not null unique
+  constraint  check_phoneNumber check (phoneNumber in ('+','1','2','3','4','5','6','7','8','9','0'))
+);
+
+create table viewofproduct
+(
+id_view int(4) auto_increment primary key,
+nameofproduct varchar(32) not null,
+id_type int(4),
+foreign key (id_type) references type_of_tovar (id_type) on delete cascade,
+id_izg int(4),
+foreign key (id_izg) references manufactor(id_izg) on delete cascade,
+id_quality int(4),
+foreign key (id_quality) references quality(id_quality) on delete cascade
+); 
+
+create table product
+(
+id_product int(4) auto_increment primary key,
+id_view int(4),
+foreign key (id_view) references viewofproduct (id_view) on delete cascade
+); 
