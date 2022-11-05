669ddc2f24247a21e5ded8549c2941d37f458ed9
NguyenMinh222 <79207246+NguyenMinh222@users.noreply.github.com>
2022-11-05 20:17:56 +0300

    Update ThirdLab.sql

--- ThirdLab.sql
+++ ThirdLab.sql
@@ -9,18 +9,31 @@ mysql> create table authorization(
     -> );
  
 create table type_of_tovar
- (id_type int(4) auto_increment primary key,
- type_of_tovar varchar(64) not null);
+ (
+ id_type int(4) auto_increment primary key,
+ type_of_tovar varchar(64) not null
+ );
  
-create table manufactor(
+create table manufactor
+(
 id_izg int(4) auto_increment primary key,
 manufactor varchar(32) not null,
 adress varchar(32) not null,
 telephoneNumber varchar(16),
- constraint check_telephoneNumber check (telephoneNumber in ('+','1','2','3','4','5','6','7','8','9','0')));
+ constraint check_telephoneNumber check (telephoneNumber in ('+','1','2','3','4','5','6','7','8','9','0'))
+);
  
  create table quality
- (id_quality int(4) auto_increment primary key,
- quality varchar(32) not null);
+ (
+ id_quality int(4) auto_increment primary key,
+ quality varchar(32) not null
+ );
  
- 
+ create table passport
+ (
+id_passport int(4) auto_increment primary key,
+dateRelease varchar(16) not null,
+ constraint check_dateRelease check (dateRelease in ('.','1','2','3','4','5','6','7','8','9','0')),
+validUntil varchar(16) not null,
+  constraint  check_validUntil check (validUntil in ('.','1','2','3','4','5','6','7','8','9','0'))
+ );
