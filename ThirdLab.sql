06c518fab86231407b599095cd03fefae7361cc1
NguyenMinh222 <79207246+NguyenMinh222@users.noreply.github.com>
2022-11-05 19:37:03 +0300

    Update ThirdLab.sql

--- ThirdLab.sql
+++ ThirdLab.sql
@@ -1,3 +1,17 @@
+mysql> create table authorization(
+    -> id_author int auto_increment primary key,
+    -> Login varchar(32) unique not null,
+    -> Password varchar(32) unique not null,
+    -> Image longblob,
+    -> Surname varchar(16) not null,
+    -> Name varchar(16) not null,
+    -> Patronimyc varchar(16)
+    -> );
+ 
+create table type_of_tovar
+ (id_type int(4) auto_increment primary key,
+ type_of_tovar varchar(64) not null);
+ 
 create table manufactor(
 id_izg int(4) auto_increment primary key,
 manufactor varchar(32) not null,
@@ -8,3 +22,5 @@ telephoneNumber varchar(16),
  create table quality
  (id_quality int(4) auto_increment primary key,
  quality varchar(32) not null);
+ 
+ 
