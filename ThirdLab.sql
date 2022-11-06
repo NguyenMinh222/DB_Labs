a963012f3838756e4ac71ee19803c855d708a76e
NguyenMinh222 <79207246+NguyenMinh222@users.noreply.github.com>
2022-11-06 20:04:05 +0300

    Update ThirdLab.sql

--- ThirdLab.sql
+++ ThirdLab.sql
@@ -19,8 +19,7 @@ create table manufactor
 id_izg int(4) auto_increment primary key,
 manufactor varchar(32) not null,
 adress varchar(32) not null,
-telephoneNumber varchar(16),
- constraint check_telephoneNumber check (telephoneNumber in ('+','1','2','3','4','5','6','7','8','9','0'))
+telephoneNumber varchar(16)
 );
  
  create table quality
